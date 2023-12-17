b0fe6c5a525603b0d2403357f297bcf150934423
Vados84 <vadimkosaservice@gmail.com>
2023-12-17 19:04:56 +0200

    hw2-js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,18 @@
 'use strict';
 
-function getElementWidth(content, padding, border) {
-  content = Number.parseFloat(content);
-  padding = Number.parseFloat(padding);
-  border = Number.parseFloat(border);
-  return content + padding * 2 + border * 2;
+function checkForSpam(message) {
+  const mes = message.toLowerCase();
+  if (mes.includes('spam') || mes.includes('sale')) {
+    return Boolean(true);
+  } else {
+    return Boolean(false);
+  }
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
